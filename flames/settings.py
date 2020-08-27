55935366f16a4b7e40c8dd1e21315b372cb31253
Nanthakumar <jjnanthakumar477@gmail.com>
2020-08-27 12:19:41 +0530

    initial commit

--- flames/settings.py
+++ flames/settings.py
@@ -127,5 +127,5 @@ STATICFILES_DIRS = (
    os.path.join(BASE_DIR, 'static'),
 )
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
